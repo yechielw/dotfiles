f6404d0b72610cc1f506375dda87fc0886db02a1
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2023-10-16 23:29:09 +0300

    Initial commit; added fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,19 @@
+# Put system-wide fish configuration entries here
+# or in .fish files in conf.d/
+# Files in conf.d can be overridden by the user
+# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
+
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+# if status is-interactive
+#   ...
+# end
+# test
+
+set fish_prompt_pwd_dir_length 0
+fish_vi_key_bindings
+alias config="git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME"
